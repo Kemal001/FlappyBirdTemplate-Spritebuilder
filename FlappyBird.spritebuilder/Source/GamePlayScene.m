3fb935a13a58f0b1a35333cda90913a63954deb0
Ayan Yenbekbay <ayan.yenb@gmail.com>
2015-07-02 17:27:48 +0600

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -12,11 +12,26 @@
     [physicsNode addChild:character];
     
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    //increment the time since the last obstacle was added;
+    timeSinceObstacle += delta;
+    
+    //check to see if two seconds have passed
+    if (timeSinceObstacle > 2.0f)
+    {
+        //add a new obstacle
+        [self addObstacle];
+        
+        //then reset the timer;
+        timeSinceObstacle = 0.0f;
+    }
+    
+    
 }
 
 // put new methods here
